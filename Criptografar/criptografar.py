adb48590c8dedcd571a09ff98447edfbbe1a1d22
Diego Ferreira <diegoferreira.silva11@gmail.com>
2022-06-28 23:01:23 -0300

    executável para o projeto Criptografar e atualização do criptografador

--- Criptografar/criptografar.py
+++ Criptografar/criptografar.py
@@ -1,38 +1,74 @@
 def cripto(frase):
     tradutor = ''
+    contador = 0
     for letra in frase:
         if letra in 'Aa':
-            tradutor += 'f'
+            tradutor += 'd'
         elif letra in 'Bb':
-            tradutor += 'j'
+            tradutor += 'e'
         elif letra in 'Cc':
-            tradutor += 'l'
+            tradutor += 'f'
         elif letra in 'Dd':
-            tradutor += '%'
+            tradutor += 'g'
         elif letra in 'Ee':
-            tradutor += 'k'
-        elif letra in 'Ff':
-            tradutor += 'y'
-        elif letra in 'Gg':
-            tradutor += '6'
-        elif letra in 'Hh':
-            tradutor += '8'
-        elif letra in 'Ii':
-            tradutor += 'x'
-        elif letra in 'Jj':
-            tradutor += '$'
-        elif letra in 'Kk':
-            tradutor += '3'
-        elif letra in 'Ll':
-            tradutor += 'm'
-        elif letra in 'Mm':
-            tradutor += 'z'
-        elif letra in 'Oo':
-            tradutor += '&'
-        elif letra in 'Uu':
-            tradutor += 'w'
-        elif letra in ' ':
             tradutor += 'h'
+        elif letra in 'Ff':
+            tradutor += 'i'
+        elif letra in 'Gg':
+            tradutor += 'j'
+        elif letra in 'Hh':
+            tradutor += 'k'
+        elif letra in 'Ii':
+            tradutor += 'l'
+        elif letra in 'Jj':
+            tradutor += 'm'
+        elif letra in 'Kk':
+            tradutor += 'n'
+        elif letra in 'Ll':
+            tradutor += 'o'
+        elif letra in 'Mm':
+            tradutor += 'p'
+        elif letra in 'Nn':
+            tradutor += 'q'
+        elif letra in 'Oo':
+            tradutor += 'r'
+        elif letra in 'Pp':
+            tradutor += 's'
+        elif letra in 'Qq':
+            tradutor += 't'
+        elif letra in 'Rr':
+            tradutor += 'u'
+        elif letra in 'Ss':
+            tradutor += 'v'
+        elif letra in 'Tt':
+            tradutor += 'w'
+        elif letra in 'Uu':
+            tradutor += 'x'
+        elif letra in 'Vv':
+            tradutor += 'y'
+        elif letra in 'Ww':
+            tradutor += 'z'
+        elif letra in 'Xx':
+            tradutor += 'a'
+        elif letra in 'Yy':
+            tradutor += 'b'
+        elif letra in 'Zz':
+            tradutor += 'c'
+        elif letra in ' ':
+            contador += 1
+            if contador == 1:
+                tradutor += '!'
+            elif contador == 2:
+                tradutor += '@'
+            elif contador == 3:
+                tradutor += '#'
+            elif contador == 4:
+                tradutor += '$'
+            elif contador == 5:
+                tradutor += '%'
+            elif contador == 6:
+                tradutor += '&'
+                contador = 0
         else:
             tradutor += letra
     return tradutor.lower()
@@ -40,37 +76,59 @@ def cripto(frase):
 def descripto(frase):
     tradutor = ''
     for letra in frase:
-        if letra in 'f':
+        if letra in 'd':
             tradutor += 'a'
-        elif letra in 'j':
+        elif letra in 'e':
             tradutor += 'b'
-        elif letra in 'l':
+        elif letra in 'f':
             tradutor += 'c'
-        elif letra in '%':
+        elif letra in 'g':
             tradutor += 'd'
-        elif letra in 'k':
-            tradutor += 'e'
-        elif letra in 'y':
-            tradutor += 'f'
-        elif letra in '6':
-            tradutor += 'g'
-        elif letra in '8':
-            tradutor += 'h'
-        elif letra in 'x':
-            tradutor += 'i'
-        elif letra in '$':
-            tradutor += 'j'
-        elif letra in '3':
-            tradutor += 'k'
-        elif letra in 'm':
-            tradutor += 'l'
-        elif letra in 'z':
-            tradutor += 'm'
-        elif letra in '&':
-            tradutor += 'o'
-        elif letra in 'w':
-            tradutor += 'u'
         elif letra in 'h':
+            tradutor += 'e'
+        elif letra in 'i':
+            tradutor += 'f'
+        elif letra in 'j':
+            tradutor += 'g'
+        elif letra in 'k':
+            tradutor += 'h'
+        elif letra in 'l':
+            tradutor += 'i'
+        elif letra in 'm':
+            tradutor += 'j'
+        elif letra in 'n':
+            tradutor += 'k'
+        elif letra in 'o':
+            tradutor += 'l'
+        elif letra in 'p':
+            tradutor += 'm'
+        elif letra in 'q':
+            tradutor += 'n'
+        elif letra in 'r':
+            tradutor += 'o'
+        elif letra in 's':
+            tradutor += 'p'
+        elif letra in 't':
+            tradutor += 'q'
+        elif letra in 'u':
+            tradutor += 'r'
+        elif letra in 'v':
+            tradutor += 's'
+        elif letra in 'w':
+            tradutor += 't'
+        elif letra in 'x':
+            tradutor += 'u'
+        elif letra in 'y':
+            tradutor += 'v'
+        elif letra in 'z':
+            tradutor += 'w'
+        elif letra in 'a':
+            tradutor += 'x'
+        elif letra in 'b':
+            tradutor += 'y'
+        elif letra in 'c':
+            tradutor += 'z'
+        elif letra in '!@#$%&':
             tradutor += ' '
         else:
             tradutor += letra
